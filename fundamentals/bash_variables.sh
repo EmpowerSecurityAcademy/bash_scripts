73ff664943797c9fb81c8339782b7bfec5017f66
sheltowt <sheltowt@gmail.com>
2016-07-05 14:34:17 -0400

    adding updates

---
+++ fundamentals/bash_variables.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# the name of the bash script
+echo $0
+
+#the user currently running the script
+echo $USER
+
+#the current line number of the script
+echo $LINENO
+
+#the process id of the current script
+echo $$
+
+#an example of creating a custom variable
+createdvariable=Exampleofcreatedcreatedvariable
+
+echo $createdvariable
